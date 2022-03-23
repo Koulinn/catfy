49d36ae4adc25b186a08476dad77d241e947f6e4
Rafael Lima <drdverzola@gmail.com>
2022-03-23 17:54:07 +0000

    Cleaned code

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import Cards from "./Cards";
 
-const { REACT_APP_CAT_API_KEY, REACT_APP_CAT_URL_BREED, REACT_APP_CAT_URL } =
-  process.env;
+const { REACT_APP_CAT_API_KEY, REACT_APP_CAT_URL } = process.env;
 
 function Sections({ categoryId }) {
   const [cats, setCats] = useState([]);
